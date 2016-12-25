ac18ff9acd815f4967d552ccc492c946029e8913
Clément Bizeau <clement.bizeau4@gmail.com>
2016-12-25 22:43:00 +0100

    manage parameter validation error

--- main.py
+++ main.py
@@ -11,25 +11,38 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+class ValidationException(Exception):
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'parameter %(name)s failed validation' % {'name': self.name}
+
+
 class Graph(object):
     """
     Handles graph request
     """
 
     def on_get(self, req, resp, stock_id, days):
-        if req.params['window'] is None:
-            raise Exception("parameter window is missing")
-        if req.params['step'] is None:
-            raise Exception("step window is missing")
-        today = date.today() - timedelta(days=1)
-        stocks = Stocks()
         try:
+            if 'window' not in req.params:
+                raise ValidationException('window')
+            if 'step' not in req.params:
+                raise ValidationException('step')
+            stocks = Stocks()
+            today = date.today() - timedelta(days=1)
             hist = stocks.getHistPrice(stock_id, int(req.params['step']), int(req.params['window']), today, int(days))
             resp.status = falcon.HTTP_200
             req.context['result'] = hist
+        except ValidationException as exc:
+            logger.exception("parameter validation")
+            raise falcon.HTTPBadRequest('validation_error', 'Parameter %(parameter)s is not valid' %
+                                        {'parameter': exc.name})
         except Exception:
             logger.exception("Error getting graph data")
-            raise falcon.HTTPInternalServerError('unknown error')
+            raise falcon.HTTPInternalServerError('unknown_error', '')
 
 
 cors = CORS(allow_all_origins=True)
